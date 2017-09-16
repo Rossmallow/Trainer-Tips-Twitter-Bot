9379737ae59cf4790db20ba41d821fb0d6c7fbe8
Ross Nelson <Rossn430@gmail.com>
2017-09-16 13:21:13 -0500

    removed config.js from the cache.

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  consumer_key:         'RsNabN270EaHGOCTSjzasCo4E',
-  consumer_secret:      'nCtj00uDJJQ59M1tXiJK6HJOICKvuGRXCuAdctLqc4wlePaWMB',
-  access_token:         '857771956053000192-EOPTTErmnO5BhTLYPyyFdfEH3MRiIUy',
-  access_token_secret:  'DFqkp9X35MUULu3DCBkzeG4ALTGS3XreXf7OONDbhvXEz'
-}
-
-module.exports = config;
